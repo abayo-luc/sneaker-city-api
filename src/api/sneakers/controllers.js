5131b18e1fccbfefa7354c8330a0ae4638cd2d3c
Luc <luc.bayo@gmail.com>
2020-10-04 10:06:06 +0200

    feature: add correct data model

--- src/api/sneakers/controllers.js
+++ src/api/sneakers/controllers.js
@@ -1,4 +1,4 @@
-import data from '../../database/data.json';
+import data from '../../database';
 
 export const getAll = (req, res) => {
   const { search, page = 1, limit = 10 } = req.query;
@@ -8,11 +8,11 @@ export const getAll = (req, res) => {
    * and slicing
    */
   let sortedData = [...data.results].sort(
-    (a, b) => new Date(b.releaseDate) - new Date(a.releaseDate)
+    (a, b) => new Date(b.release_date) - new Date(a.release_date)
   );
   if (search) {
     sortedData = sortedData.filter((item) =>
-      `${item.brand} ${item.model} ${item.title}`
+      `${item.brand_name} ${item.model} ${item.name}`
         .toLocaleLowerCase()
         .split(' ')
         .includes(search.trim()?.toLocaleLowerCase())
@@ -24,7 +24,7 @@ export const getAll = (req, res) => {
 
 export const getOne = (req, res) => {
   const { id } = req.params;
-  const sneaker = data.results.find((item) => item.id === id);
+  const sneaker = data.results.find((item) => item.id == id);
   if (!sneaker) {
     return res.status(404).json({ error: 'Record not found' });
   }
